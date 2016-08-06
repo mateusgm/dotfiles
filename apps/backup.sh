39d3f75551cfa00157f6b76861f2088474e8edb0
Mateus Galvão <mateus@uololou.com.br>
2016-08-06 17:00:44 +0200

    Mudancas

--- apps/backup.sh
+++ apps/backup.sh
@@ -11,4 +11,6 @@ rsync -Crvazu --delete \
   --exclude=cache/ \
   --exclude=tmp/ \
   --exclude=bundle/ \
+  --exclude=*.crdownload \
+  --exclude=.Trash-1000/ \
   /media/arquivos/ /media/mateus/4E0D13D72588E118/Mateus/
