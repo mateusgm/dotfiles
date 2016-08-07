da8cb8d48b57a7bc6b7a150940e9aa53c7561e12
root <mateus@uololou.com.br>
2016-08-07 18:16:32 +0000

    Server recipes

--- recipes/server/ruby.sh
+++ recipes/server/ruby.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/bash
+
+sudo apt-get install -y nodejs
+
 curl -L https://get.rvm.io | bash -s stable --ruby
 source ~/.rvm/scripts/rvm
+
 gem install bundler
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> ~/.bash_profile
